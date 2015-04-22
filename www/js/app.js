d8e305e10c7bc0b7ac45e52eb0948da1b572eb6b
phaggood <patrick.haggood@gmail.com>
2015-04-21 22:18:18 -0400

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,32 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .controller('BarcodeCtrl', function ($scope, $cordovaBarcodeScanner) {
+
+        $scope.getPicture = function () {
+            $cordovaBarcodeScanner.scan().then(function (imageData) {
+                console.log("success");
+                console.log("Barcode Format -> " + imageData.format);
+                console.log("Cancelled -> " + imageData.cancelled);
+            }, function (error) {
+                console.log("An error happened -> " + error);
+            });
+        };
+    });
